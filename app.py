537098f108e7995105612a9e73a182cf540d4a90
wzk0 <729301277@qq.com>
2023-05-28 06:06:21 +0800

    为视频页面添加了收藏/下载/丢屎的按钮 修改了一些链接

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ def index():
 	name=cookie.get('name')
 	pwd=cookie.get('pwd')
 	if name==None:
-		return render_template('index.html',word='🥲目前还未注册 or 登陆, 无法使用收藏功能!',data=x8.analysis_page(1),page=True,next_page=2,user=False)
+		return render_template('index.html',word='🤖目前还未注册 or 登陆, 无法使用收藏功能!',data=x8.analysis_page(1),page=True,next_page=2,user=False)
 	else:
-		return render_template('index.html',word='🎉欢迎! %s'%name,data=x8.analysis_page(1),page=True,next_page=2,user=True)
+		return render_template('index.html',word='🌈欢迎! %s'%name,data=x8.analysis_page(1),page=True,next_page=2,user=True)
 
 @app.route('/video/<string:vid>')
 def video(vid):
@@ -19,7 +19,7 @@ def video(vid):
 	name=cookie.get('name')
 	pwd=cookie.get('pwd')
 	if name==None:
-		return render_template('video.html',word='🥲目前还未注册 or 登陆, 无法使用收藏功能!',data=x8.analysis_video(vid),vid=vid,user=False)
+		return render_template('video.html',word='👽目前还未注册 or 登陆, 无法使用收藏功能!',data=x8.analysis_video(vid),vid=vid,user=False)
 	else:
 		return render_template('video.html',word='🎉欢迎! %s'%name,data=x8.analysis_page(1),vid=vid,user=True)
 
@@ -30,11 +30,11 @@ def page(page):
 	pwd=cookie.get('pwd')
 	if int(page)==1:
 		if name==None:
-			return render_template('index.html',word='🥲目前还未注册 or 登陆, 无法使用收藏功能!',data=x8.analysis_page(1),page=True,next_page=2,user=False)
+			return render_template('index.html',word='👾目前还未注册 or 登陆, 无法使用收藏功能!',data=x8.analysis_page(1),page=True,next_page=2,user=False)
 		else:
-			return render_template('index.html',word='🎉欢迎! %s'%name,data=x8.analysis_page(1),page=True,next_page=2,user=True)
+			return render_template('index.html',word='🔥欢迎! %s'%name,data=x8.analysis_page(1),page=True,next_page=2,user=True)
 	else:
 		if name==None:
-			return render_template('index.html',word='🥲目前还未注册 or 登陆, 无法使用收藏功能!',data=x8.analysis_page(page),page=False,next_page=int(page)+1,previous_page=int(page)-1,user=False)
+			return render_template('index.html',word='👻目前还未注册 or 登陆, 无法使用收藏功能!',data=x8.analysis_page(page),page=False,next_page=int(page)+1,previous_page=int(page)-1,user=False)
 		else:
-			return render_template('index.html',word='🎉欢迎! %s'%name,data=x8.analysis_page(page),page=False,next_page=int(page)+1,previous_page=int(page)-1,user=True)
+			return render_template('index.html',word='⭐欢迎! %s'%name,data=x8.analysis_page(page),page=False,next_page=int(page)+1,previous_page=int(page)-1,user=True)
